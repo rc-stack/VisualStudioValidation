f2abd2024723dd52e0d4c10e18f949da38cf661d
rc-stack <rc@before.health>
2025-12-17 10:28:22 -0500

    Rollback to previous Customer

--- src/main/java/com/example/demo/domain/enums/models/Customer.java
+++ src/main/java/com/example/demo/domain/enums/models/Customer.java
@@ -28,7 +28,7 @@ public class Customer {
     
     @NotBlank(message = "Phone is mandatory")
     
-    @Size(min = 2, max = 30, message = "Let's make a rollback")
+    @Size(min = 2, max = 30, message = "Phone must be exact 10 digits")
     private String phone;
 
 }
